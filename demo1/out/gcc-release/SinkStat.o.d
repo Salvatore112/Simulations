eb85d6e9daa4b4c9d59e8aa49701a0f7e1245639
Pavel Averin <pasha2281337alfa@gmail.com>
2025-04-25 19:12:03 +0300

    feat: add second task

---
+++ demo1/out/gcc-release/SinkStat.o.d
@@ -0,0 +1,7 @@
+out/gcc-release//SinkStat.o: SinkStat.cc SinkStat.h \
+ /home/pasha/omnetpp-6.1/include/omnetpp.h ../queueinglib/Sink.h \
+ ../queueinglib/QueueingDefs.h
+SinkStat.h:
+/home/pasha/omnetpp-6.1/include/omnetpp.h:
+../queueinglib/Sink.h:
+../queueinglib/QueueingDefs.h:
